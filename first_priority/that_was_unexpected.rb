21c1ef4bd88c14f0f04f60629f2b764e8c88b271
Wesley Chen <weschen1996@gmail.com>
2020-01-22 22:34:28 +0000

    Automatically backed up by Learn

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,11 +1,13 @@
+require 'pry'
+
 class Person
   attr_accessor :name, :candy_lover
-
+  
   def initialize(name, candy_lover)
     @name = name
     @candy_lover = candy_lover
   end
-
+  
   def tall?
     true
   end
@@ -24,6 +26,7 @@ class Person
     else
       10.times do
         "not a ballin candy lover"
+      end
     end
   end
 end
